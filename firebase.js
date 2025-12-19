0c471c5dfd4f12c80f4973a6bbd1ec672ca2b418
praveenmanthiramoorthi <praveenmanthiramoorthi@gmail.com>
2025-12-19 14:15:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,13 @@
 // firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
+// Initialize Firebase Admin using environment variables
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const db = admin.firestore();
